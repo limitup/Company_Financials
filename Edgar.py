0ccbceb58940f0adb107134749453bb966762c67
kurtiskerstein <krkerstein@gmail.com>
2017-10-27 15:33:41 -0500

    testing some code

--- Edgar.py
+++ Edgar.py
@@ -1,6 +1,17 @@
 import requests
+import json
 
 
-API_Key = Secret_Key
+# website =  'http://edgaronline.api.mashery.com/v2/companies.json?primarysymbols='FB'&appkey={API_Key}'
+# payload = {'appkey' = 'Secret_key'}
+# r = requests.get('http://edgaronline.api.mashery.com/v2/companies.json', params=payload)
 
-# http://edgaronline.api.mashery.com/v2/companies.json?primarysymbols=msft&appkey={API_Key}
+
+# http://financials.morningstar.com/income-statement/is.html?t=X&ops=clear
+payload = {'t':'X', 'ops': 'clear'}
+r = requests.get('http://financials.morningstar.com/income-statement/is.html', params=payload)
+data = r.json
+print(data)
+
+json_data = json.dumps(data)
+print(json_data)
