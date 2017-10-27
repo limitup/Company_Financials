7e03b554c3193b3c08456206f90f0a4afe2a3a88
kurtiskerstein <krkerstein@gmail.com>
2017-10-27 15:07:41 -0500

    updates to file

---
+++ Edgar.py
@@ -0,0 +1,6 @@
+import requests
+
+
+API_Key = Secret_Key
+
+# http://edgaronline.api.mashery.com/v2/companies.json?primarysymbols=msft&appkey={API_Key}
---
+++ README.md
@@ -0,0 +1,3 @@
+Pull symbols from PDF file for the Russell 2000
+
+extract data from Edgar API with requests library
